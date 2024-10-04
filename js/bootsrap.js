58f4b11890a9eacfd655debf1c41c89da0d48b72
Muhfhri <muhammadfachrialhusein.1270@gmail.com>
2024-10-04 19:40:25 +0700

    Add files via upload

---
+++ js/bootsrap.js
@@ -0,0 +1,3 @@
+// script.js
+const bootstrapCssUrl = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"; // Ganti versi sesuai kebutuhan
+document.getElementById("bootstrap-css").setAttribute("href", bootstrapCssUrl);
